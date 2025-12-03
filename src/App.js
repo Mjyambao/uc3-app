57cf05065dec34cd3f00e0a5018f39ce95ccb574
Michael Yambao <michael.j.yambao@accenture.com>
2025-12-03 09:32:37 +0800

    Initial Files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test app for Azure DevOps Use Case 3.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
